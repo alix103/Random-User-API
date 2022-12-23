88361c0e8ae343b0cd25bd32f1f7b13135537f4d
alix103 <atbermudez96@misena.edu.co>
2022-12-23 18:13:54 -0500

    Terminación consumo de la API Random User

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,6 @@ const select = document.querySelector('.select-gender');
 const female = document.querySelector('.female');
 const male = document.querySelector('.male');
 
-
-
 //const getRandomInt = (min, max) => {
     //return Math.floor(Math.random()*(max - min)) + min;
 //}
@@ -26,40 +24,43 @@ select.addEventListener('change',  (event)=> {
     }
 })
     
-
 const selectFemale = async () => {
     const res = await fetch(`https://randomuser.me/api/?gender=female`)
     const data = await res.json();
-    fetchData(data);
+    dataPerson(data);
 }
 const selectMale = async () => {
     const res = await fetch(`https://randomuser.me/api/?gender=male`)
     const data = await res.json();
-    fetchData(data);
+    dataPerson(data);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchData();
 })
 
-const fetchData = async (id) =>{
+const dataPerson = (data) =>{
+    const user = {
+        img : data.results[0].picture.large,
+        nombre : data.results[0].name.first,
+        apellido : data.results[0].name.last,
+        genero : data.results[0].gender,
+        cumpleanos : data.results[0].dob.date,
+        email : data.results[0].email,
+        telefono : data.results[0].phone
+    }
+    
+    pintarCard(user);
+}
+
+const fetchData = async () =>{
     try {
-        const res = await fetch(`https://randomuser.me/api/?gender=${id}`);
+        const res = await fetch(`https://randomuser.me/api/`);
         const data = await res.json();
 
-        console.log(data);
+        dataPerson(data);
 
-        const user = {
-            img : data.results[0].picture.large,
-            nombre : data.results[0].name.first,
-            apellido : data.results[0].name.last,
-            genero : data.results[0].gender,
-            cumpleanos : data.results[0].dob.date,
-            email : data.results[0].email,
-            telefono : data.results[0].phone
-        }
-        
-        pintarCard(user);
+        console.log(data);
     } catch (error) {
         console.log(error);
     }
